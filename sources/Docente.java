4c14a3027e3b6fcd9a901dd09b5722979410804c
Ezequiel <quielemao@gmail.com>
2019-11-12 15:24:47 -0300

    iniciando as classes

---
+++ sources/Docente.java
@@ -0,0 +1,10 @@
+public class Docente {
+    public int codigo;
+    public String nome;
+    public Boll coordenador;		
+    public Calendar dataNascimento;
+    public Calendar dataIngresso;
+    public HashMap<cod, Publicacao> Publicacoes;
+
+    public quaisSaoSuasPublicacoes();
+}
---
+++ sources/Sistema.java
@@ -0,0 +1,14 @@
+import java.util.List;
+// import java.util.Calendar;
+// import java.util.Date;
+// import java.util.Scanner;
+// import java.util.HashMap;
+// import java.util.Map;
+
+public class Sistema {
+    public List<Docente> docentes;
+    
+    public static void main(String args[]) {
+
+    }
+}
